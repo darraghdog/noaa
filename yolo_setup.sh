203559af50eda023dbefff81fb53a2868d637fe7
Ubuntu <ubuntu@ip-172-31-34-84.eu-west-1.compute.internal>
2017-04-14 09:02:01 +0000

    Blackened Images

--- yolo_setup.sh
+++ yolo_setup.sh
@@ -17,3 +17,4 @@ cp yolo-voc-seals544small-blk.cfg darknet/cfg/yolo-voc-seals544small-blk.cfg
 # ./darknet detector test cfg/voc.seals.data cfg/yolo-voc-seals544small.cfg backup/yolo-voc-seals544small_6000.weights seals/JPEGImages/591_20.jpg
 
 # nohup ./darknet detector train cfg/voc.seals.data cfg/yolo-voc-seals544small-blk.cfg darknet19_448.conv.23  > nohup544small-blk.out 2>&1&
+# nohup ./darknet detector valid cfg/voc.seals.data cfg/yolo-voc-seals544small-blk.cfg whichever_weights  > nohup544small-blkpred.out 2>&1&
